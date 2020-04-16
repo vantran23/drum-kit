5cdc70de3633b67bd20345bb6483515e111a1805
Pursuit <pursuit@Pursuits-Air-2.attlocal.net>
2020-04-15 17:27:33 -0700

    add keyboard keys to play drums

--- index.js
+++ index.js
@@ -1,50 +1,69 @@
 let numOfDrumBtns = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numOfDrumBtns; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let buttonInnerHTML = this.innerHTML;
 
-    let buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHTML);
 
-        switch (buttonInnerHtml) {
-            case "w":
-                let crash = new Audio("sounds/crash.mp3");
-                crash.play();
-            break;
-
-            case "a":
-                let kickBass = new Audio ("sounds/kick-bass.mp3");
-                kickBass.play();
-            break;
-
-            case "s":
-                let snare = new Audio ("sounds/snare.mp3");
-                snare.play();
-            break;
-
-            case "d":
-                let tom1 = new Audio ("sounds/tom-1.mp3");
-                tom1.play();
-            break;
-
-            case "j":
-                let tom2 = new Audio ("sounds/tom-2.mp3");
-                tom2.play();
-            break;
-
-            case "k":
-                let tom3 = new Audio ("sounds/tom-3.mp3");
-                tom3.play();
-            break;
-
-            case "l":
-                let tom4 = new Audio ("sounds/tom-4.mp3");
-                tom4.play();
-            break;
-
-            default: console.log(buttonInnerHtml);
-        
-        }
-
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      let kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    case "s":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonInnerHtml);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
