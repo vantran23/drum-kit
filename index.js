d5953c75833a104d99b9e678f80810eb975046e6
Pursuit <pursuit@Pursuits-Air-2.attlocal.net>
2020-04-15 17:15:42 -0700

    add sound to drums

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
 let numOfDrumBtns = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numOfDrumBtns; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-   let audio = new Audio("sounds/tom-1.mp3");
-   audio.play();
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    let buttonInnerHtml = this.innerHTML;
+
+        switch (buttonInnerHtml) {
+            case "w":
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play();
+            break;
+
+            case "a":
+                let kickBass = new Audio ("sounds/kick-bass.mp3");
+                kickBass.play();
+            break;
+
+            case "s":
+                let snare = new Audio ("sounds/snare.mp3");
+                snare.play();
+            break;
+
+            case "d":
+                let tom1 = new Audio ("sounds/tom-1.mp3");
+                tom1.play();
+            break;
+
+            case "j":
+                let tom2 = new Audio ("sounds/tom-2.mp3");
+                tom2.play();
+            break;
+
+            case "k":
+                let tom3 = new Audio ("sounds/tom-3.mp3");
+                tom3.play();
+            break;
+
+            case "l":
+                let tom4 = new Audio ("sounds/tom-4.mp3");
+                tom4.play();
+            break;
+
+            default: console.log(buttonInnerHtml);
+        
+        }
+
   });
 }
 
